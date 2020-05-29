66e558aa61dafe0f3283fe22e6f031c36578efa5
thepluxuryprince <mr.stukanovmlg@gmail.com>
2020-05-29 22:06:18 +0700

    Added fprint_move function

--- src/board_print.c
+++ src/board_print.c
@@ -1,12 +1,12 @@
 #include "board_print.h"
 #include "board.h"
 #include <stdio.h>
-void print_board(char (*chessboard)[8], const int hor, const int vert)
+void print_board(char (*chessboard)[8])
 {
-    printf(" | a  b  c  d  e  f  g  h |\n____________________________");
-    for (int j = vert - 1; j >= 0; j--) {
+    printf("\n | a  b  c  d  e  f  g  h |\n____________________________");
+    for (int j = 8 - 1; j >= 0; j--) {
         printf("\n%d|", j + 1);
-        for (int i = 0; i < hor; i++) {
+        for (int i = 0; i < 8; i++) {
             printf(" %c ", chessboard[i][j]);
             if (i == 7) {
                 printf("|%d", j + 1);
@@ -16,8 +16,7 @@ void print_board(char (*chessboard)[8], const int hor, const int vert)
     printf("\n____________________________\n | a  b  c  d  e  f  g  h |");
     printf("\n ход:");
 }
-void file_print_board(
-        char word, int num, char word1, int num1, char (*chessboard)[8])
+void file_print_board(char (*chessboard)[8])
 {
     FILE* file;
     file = fopen("game_history.txt", "a");
@@ -25,7 +24,8 @@ void file_print_board(
         printf("Ошибка");
         exit(1);
     }
-    fprintf(file, " | a  b  c  d  e  f  g  h |\n____________________________");
+    fprintf(file,
+            "\n | a  b  c  d  e  f  g  h |\n____________________________");
     for (int j = 8 - 1; j >= 0; j--) {
         fprintf(file, "\n%d|", j + 1);
         for (int i = 0; i < 8; i++) {
@@ -37,6 +37,16 @@ void file_print_board(
     }
     fprintf(file,
             "\n____________________________\n | a  b  c  d  e  f  g  h |");
+    fclose(file);
+}
+void fprint_move(char word, int num, char word1, int num1)
+{
+    FILE* file;
+    file = fopen("game_history.txt", "a");
+    if (file == NULL) {
+        printf("Ошибка");
+        exit(1);
+    }
     fprintf(file, "\n ход:");
     fprintf(file, "\n");
     fprintf(file, " %c%d %c%d ", word, num, word1, num1);
--- src/board_print.h
+++ src/board_print.h
@@ -1,7 +1,7 @@
 #ifndef BOARD_PRINT_H
 #define BOARD_PRINT_H
 #include <stdlib.h>
-void print_board(char (*chessboard)[8], const int hor, const int vert);
-void file_print_board(
-        char word, int num, char word1, int num1, char (*chessboard)[8]);
+void print_board(char (*chessboard)[8]);
+void file_print_board(char (*chessboard)[8]);
+void fprint_move(char word, int num, char word1, int num1);
 #endif

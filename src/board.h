a48291632a68c952db5673f02b591050be5b50c2
thepluxuryprince <mr.stukanovmlg@gmail.com>
2020-05-29 22:06:36 +0700

    update board.h

--- src/board.h
+++ src/board.h
@@ -1,5 +1,7 @@
 #ifndef BOARD_H
 #define BOARD_H
+#include <math.h>
 #include <stdio.h>
-int fill_board(char (*chessboard)[8], const int hor, const int vert);
+void fill_board(char (*chessboard)[8]);
+int move(int w, int n, int w1, int n1, char (*chessboard)[8]);
 #endif

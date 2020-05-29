dfd4858f0d12277223fef7ee535e9e60ef35c70b
thepluxuryprince <mr.stukanovmlg@gmail.com>
2020-05-29 22:04:16 +0700

    Added move function

--- src/board.c
+++ src/board.c
@@ -1,9 +1,9 @@
 #include "board.h"
-int fill_board(char (*chessboard)[8], const int hor, const int vert)
+#include "board_print.h"
+void fill_board(char (*chessboard)[8])
 {
-    /*Заполнение доски пробелами*/
-    for (int j = 0; j < vert; j++) {
-        for (int i = 0; i < hor; i++) {
+    for (int j = 0; j < 8; j++) {
+        for (int i = 0; i < 8; i++) {
             chessboard[i][j] = 32;
         }
     }
@@ -42,5 +42,200 @@ int fill_board(char (*chessboard)[8], const int hor, const int vert)
     chessboard[5][6] = 'p';
     chessboard[6][6] = 'p';
     chessboard[7][6] = 'p';
-    return 0;
+}
+int move(int w, int n, int w1, int n1, char (*chessboard)[8])
+{
+    if (chessboard[w][n] == 'P') // white pawns
+    {
+        if (chessboard[w1][n1] == ' ' && w == w1 && abs(n1 - n) <= 2) {
+            chessboard[w1][n1] = chessboard[w][n];
+            chessboard[w][n] = ' ';
+            return 1;
+        }
+        if (chessboard[w1][n1] != ' ' && chessboard[w1][n1] >= 97
+            && chessboard[w1][n1] <= 122
+            && ((n - 1 == n1 && w - 1 == w1) || (n - 1 == n1 && w + 1 == w1))) {
+            chessboard[w1][n1] = chessboard[w][n];
+            chessboard[w][n] = ' ';
+            return 1;
+        }
+        return -1;
+    }
+    if (chessboard[w][n] == 'p') // black pawns
+    {
+        if (chessboard[w1][n1] == ' ' && w == w1 && abs(n1 - n) <= 2) {
+            chessboard[w1][n1] = chessboard[w][n];
+            chessboard[w][n] = ' ';
+            return 1;
+        }
+        if (chessboard[w1][n1] != ' ' && chessboard[w1][n1] >= 65
+            && chessboard[w1][n1] <= 90
+            && ((n - 1 == n1 && (w - 1 == w1 || w + 1 == w1)))) {
+            chessboard[w1][n1] = chessboard[w][n];
+            chessboard[w][n] = ' ';
+            return 1;
+        }
+        return -1;
+    }
+    if (chessboard[w][n] == 'R') // white rooks
+    {
+        if (chessboard[w1][n1] == ' ' && (w == w1 || n == n1)) {
+            chessboard[w1][n1] = chessboard[w][n];
+            chessboard[w][n] = ' ';
+            return 1;
+        }
+        if (chessboard[w1][n1] != ' ' && chessboard[w1][n1] >= 97
+            && chessboard[w1][n1] <= 122 && (w == w1 || n == n1)) {
+            chessboard[w1][n1] = chessboard[w][n];
+            chessboard[w][n] = ' ';
+            return 1;
+        }
+        return -1;
+    }
+    if (chessboard[w][n] == 'r') // black rooks
+    {
+        if (chessboard[w1][n1] == ' ' && (w == w1 || n == n1)) {
+            chessboard[w1][n1] = chessboard[w][n];
+            chessboard[w][n] = ' ';
+            return 1;
+        }
+        if (chessboard[w1][n1] != ' ' && chessboard[w1][n1] >= 65
+            && chessboard[w1][n1] <= 90 && (w == w1 || n == n1)) {
+            chessboard[w1][n1] = chessboard[w][n];
+            chessboard[w][n] = ' ';
+            return 1;
+        }
+        return -1;
+    }
+    if (chessboard[w][n] == 'N') // while knight (horse)
+    {
+        if (chessboard[w1][n1] == ' '
+            && (pow(w1 - w, 2) + pow(n1 - n, 2)) == 5) {
+            chessboard[w1][n1] = chessboard[w][n];
+            chessboard[w][n] = ' ';
+            return 1;
+        }
+        if (chessboard[w1][n1] != ' ' && chessboard[w1][n1] >= 97
+            && chessboard[w1][n1] <= 122
+            && (pow(w1 - w, 2) + pow(n1 - n, 2)) == 5) {
+            chessboard[w1][n1] = chessboard[w][n];
+            chessboard[w][n] = ' ';
+            return 1;
+        }
+        return -1;
+    }
+    if (chessboard[w][n] == 'n') // black knight (horse)
+    {
+        if (chessboard[w1][n1] == ' '
+            && (pow(w1 - w, 2) + pow(n1 - n, 2)) == 5) {
+            chessboard[w1][n1] = chessboard[w][n];
+            chessboard[w][n] = ' ';
+            return 1;
+        }
+        if (chessboard[w1][n1] != ' ' && chessboard[w1][n1] >= 65
+            && chessboard[w1][n1] <= 90
+            && (pow(w1 - w, 2) + pow(n1 - n, 2)) == 5) {
+            chessboard[w1][n1] = chessboard[w][n];
+            chessboard[w][n] = ' ';
+            return 1;
+        }
+        return -1;
+    }
+    if (chessboard[w][n] == 'B') // bishop white (elephant)
+    {
+        if (chessboard[w1][n1] == ' ' && abs(n1 - n) == abs(w1 - w)) {
+            chessboard[w1][n1] = chessboard[w][n];
+            chessboard[w][n] = ' ';
+            return 1;
+        }
+        if (chessboard[w1][n1] != ' ' && chessboard[w1][n1] >= 97
+            && chessboard[w1][n1] <= 122 && abs(n1 - n) == abs(w1 - w)) {
+            chessboard[w1][n1] = chessboard[w][n];
+            chessboard[w][n] = ' ';
+            return 1;
+        }
+        return -1;
+    }
+    if (chessboard[w][n] == 'b') // bishop black (elephant)
+    {
+        if (chessboard[w1][n1] == ' ' && abs(n1 - n) == abs(w1 - w)) {
+            chessboard[w1][n1] = chessboard[w][n];
+            chessboard[w][n] = ' ';
+            return 1;
+        }
+        if (chessboard[w1][n1] != ' ' && chessboard[w1][n1] >= 65
+            && chessboard[w1][n1] <= 90 && abs(n1 - n) == abs(w1 - w)) {
+            chessboard[w1][n1] = chessboard[w][n];
+            chessboard[w][n] = ' ';
+            return 1;
+        }
+        return -1;
+    }
+    if (chessboard[w][n] == 'Q') // white queen (ferz')
+    {
+        if (chessboard[w1][n1] == ' '
+            && (w == w1 || n == n1 || (abs(n1 - n) == abs(w1 - w)))) {
+            chessboard[w1][n1] = chessboard[w][n];
+            chessboard[w][n] = ' ';
+            return 1;
+        }
+        if (chessboard[w1][n1] != ' '
+            && (w == w1 || n == n1 || (abs(n1 - n) == abs(w1 - w)))
+            && chessboard[w1][n1] >= 97 && chessboard[w1][n1] <= 122) {
+            chessboard[w1][n1] = chessboard[w][n];
+            chessboard[w][n] = ' ';
+            return 1;
+        }
+        return -1;
+    }
+    if (chessboard[w][n] == 'q') // black queen (ferz')
+    {
+        if (chessboard[w1][n1] == ' '
+            && (w == w1 || n == n1 || (abs(n1 - n) == abs(w1 - w)))) {
+            chessboard[w1][n1] = chessboard[w][n];
+            chessboard[w][n] = ' ';
+            return 1;
+        }
+        if (chessboard[w1][n1] != ' '
+            && (w == w1 || n == n1 || (abs(n1 - n) == abs(w1 - w)))
+            && chessboard[w1][n1] >= 65 && chessboard[w1][n1] <= 97) {
+            chessboard[w1][n1] = chessboard[w][n];
+            chessboard[w][n] = ' ';
+            return 1;
+        }
+        return -1;
+    }
+    if (chessboard[w][n] == 'K') // White king
+    {
+        if (chessboard[w1][n1] == ' '
+            && (abs(n1 - n) == 1 || abs(w1 - w) == 1)) {
+            chessboard[w1][n1] = chessboard[w][n];
+            chessboard[w][n] = ' ';
+            return 1;
+        }
+        if (chessboard[w1][n1] != ' ' && (abs(n1 - n) == 1 || abs(w1 - w) == 1)
+            && chessboard[w1][n1] >= 97 && chessboard[w1][n1] <= 122) {
+            chessboard[w1][n1] = chessboard[w][n];
+            chessboard[w][n] = ' ';
+            return 1;
+        }
+        return -1;
+    }
+    if (chessboard[w][n] == 'k') // black king
+    {
+        if (chessboard[w1][n1] == ' '
+            && (abs(n1 - n) == 1 || abs(w1 - w) == 1)) {
+            chessboard[w1][n1] = chessboard[w][n];
+            chessboard[w][n] = ' ';
+            return 1;
+        }
+        if (chessboard[w1][n1] != ' ' && (abs(n1 - n) == 1 || abs(w1 - w) == 1)
+            && chessboard[w1][n1] >= 65 && chessboard[w1][n1] <= 90) {
+            chessboard[w1][n1] = chessboard[w][n];
+            chessboard[w][n] = ' ';
+            return 1;
+        }
+        return -1;
+    }
+    return -1;
 }
--- src/main.c
+++ src/main.c
@@ -3,150 +3,159 @@
 int main()
 {
     char chessboard[8][8];
-    const int h = 8;  //горизонталь
-    const int v = 8;  //вертикаль
-    char word, word1; //буквы хода
-    int num, num1;    //числа хода
-    int flag;
-    // int n, n1, w, w1; //числа для движения
+    struct input {
+        char word[2];
+        int num[2];
+    };
+    int flag, check = 0;
+    int n, n1, w, w1; //числа для движения
 
-    fill_board(chessboard, h, v); //заполняем пробелами доску
-    int game = 1;
-    while (game) {
-        flag++;
-        print_board(chessboard, h, v);
-        scanf("%c %d %c %d", &word, &num, &word1, &num1);
-        file_print_board(word, num, word1, num1, chessboard);
-        if (word == 'a') {
-            // w =1;
-            flag++;
-        }
-        if (word == 'b') {
-            // w =2;
-            flag++;
-        }
-        if (word == 'c') {
-            // w =3;
-            flag++;
-        }
-        if (word == 'd') {
-            // w =4;
-            flag++;
-        }
-        if (word == 'e') {
-            // w =5;
-            flag++;
-        }
-        if (word == 'f') {
-            // w =6;
-            flag++;
-        }
-        if (word == 'g') {
-            // w =7;
-            flag++;
-        }
-        if (word == 'h') {
-            // w =8;
-            flag++;
-        }
-        if (num == 1) {
-            // n =8;
-            flag++;
-        }
-        if (num == 2) {
-            // n =7;
-            flag++;
-        }
-        if (num == 3) {
-            // n =6;
-            flag++;
-        }
-        if (num == 4) {
-            // n =5;
-            flag++;
-        }
-        if (num == 5) {
-            // n =4;
-            flag++;
-        }
-        if (num == 6) {
-            // n =3;
-            flag++;
-        }
-        if (num == 7) {
-            // n =2;
-            flag++;
-        }
-        if (num == 8) {
-            // n =1;
-            flag++;
-        }
-        if (word1 == 'a') {
-            // w1 =1;
-            flag++;
-        }
-        if (word1 == 'b') {
-            // w1 =2;
-            flag++;
-        }
-        if (word1 == 'c') {
-            // w1 =3;
-            flag++;
-        }
-        if (word1 == 'd') {
-            // w1 =4;
-            flag++;
-        }
-        if (word1 == 'e') {
-            // w1 =5;
-            flag++;
-        }
-        if (word1 == 'f') {
-            // w1 =6;
-            flag++;
-        }
-        if (word1 == 'g') {
-            // w1 =7;
-            flag++;
-        }
-        if (word1 == 'h') {
-            // w1 =8;
-            flag++;
-        }
-        if (num1 == 1) {
-            // n1 =8;
-            flag++;
-        }
-        if (num1 == 2) {
-            // n1 =7;
-            flag++;
-        }
-        if (num1 == 3) {
-            // n1 =6;
-            flag++;
-        }
-        if (num1 == 4) {
-            // n1 =5;
-            flag++;
-        }
-        if (num1 == 5) {
-            // n1 =4;
-            flag++;
-        }
-        if (num1 == 6) {
-            // n1 =3;
-            flag++;
-        }
-        if (num1 == 7) {
-            // n1 =2;
-            flag++;
-        }
-        if (num1 == 8) {
-            // n1 =1;
-            flag++;
-        }
-        if (flag != 4) {
-            printf("Wrong input\n");
+    fill_board(chessboard); //заполняем пробелами доску
+    print_board(chessboard);
+    file_print_board(chessboard);
+    while (1) {
+        for (flag = 0; flag != 4;) {
+            scanf("%c%d%c%d", &word, &num, &word1, &num1);
+            fprint_move(word, num, word1, num1);
+            if (word == 'a') {
+                w = 0;
+                flag++;
+            }
+            if (word == 'b') {
+                w = 1;
+                flag++;
+            }
+            if (word == 'c') {
+                w = 2;
+                flag++;
+            }
+            if (word == 'd') {
+                w = 3;
+                flag++;
+            }
+            if (word == 'e') {
+                w = 4;
+                flag++;
+            }
+            if (word == 'f') {
+                w = 5;
+                flag++;
+            }
+            if (word == 'g') {
+                w = 6;
+                flag++;
+            }
+            if (word == 'h') {
+                w = 7;
+                flag++;
+            }
+            if (num == 1) {
+                n = 0;
+                flag++;
+            }
+            if (num == 2) {
+                n = 1;
+                flag++;
+            }
+            if (num == 3) {
+                n = 2;
+                flag++;
+            }
+            if (num == 4) {
+                n = 3;
+                flag++;
+            }
+            if (num == 5) {
+                n = 4;
+                flag++;
+            }
+            if (num == 6) {
+                n = 5;
+                flag++;
+            }
+            if (num == 7) {
+                n = 6;
+                flag++;
+            }
+            if (num == 8) {
+                n = 7;
+                flag++;
+            }
+            if (word1 == 'a') {
+                w1 = 0;
+                flag++;
+            }
+            if (word1 == 'b') {
+                w1 = 1;
+                flag++;
+            }
+            if (word1 == 'c') {
+                w1 = 2;
+                flag++;
+            }
+            if (word1 == 'd') {
+                w1 = 3;
+                flag++;
+            }
+            if (word1 == 'e') {
+                w1 = 4;
+                flag++;
+            }
+            if (word1 == 'f') {
+                w1 = 5;
+                flag++;
+            }
+            if (word1 == 'g') {
+                w1 = 6;
+                flag++;
+            }
+            if (word1 == 'h') {
+                w1 = 7;
+                flag++;
+            }
+            if (num1 == 1) {
+                n1 = 0;
+                flag++;
+            }
+            if (num1 == 2) {
+                n1 = 1;
+                flag++;
+            }
+            if (num1 == 3) {
+                n1 = 2;
+                flag++;
+            }
+            if (num1 == 4) {
+                n1 = 3;
+                flag++;
+            }
+            if (num1 == 5) {
+                n1 = 4;
+                flag++;
+            }
+            if (num1 == 6) {
+                n1 = 5;
+                flag++;
+            }
+            if (num1 == 7) {
+                n1 = 6;
+                flag++;
+            }
+            if (num1 == 8) {
+                n1 = 7;
+                flag++;
+            }
+            if (flag != 4) {
+                printf("Wrong input\n");
+                flag = 0;
+            }
+            flag = 0;
+            check = move(w, n, w1, n1, chessboard);
+            if (check != 1) {
+                printf("Wrong move\n");
+            }
+            print_board(chessboard);
+            file_print_board(chessboard);
         }
     }
     return 0;
